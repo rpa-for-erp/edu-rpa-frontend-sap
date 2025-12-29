3fc9d556e23926f4859107f8cc85bca6d1f1e52a
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2025-12-29 09:33:23 +0700

    feat/highlayer translate

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Box, Center, Heading, Text, Button } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
-import { useTranslation } from 'next-i18next';
-import { GetStaticProps } from 'next';
-import { getStaticTranslations } from '@/utils/i18n';
+import React from "react";
+import { Box, Center, Heading, Text, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
+import { GetStaticProps } from "next";
+import { getStaticTranslations } from "@/utils/i18n";
 
 const NotFound = () => {
   const router = useRouter();
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
   return (
     <Center height="100vh">
       <Box textAlign="center">
@@ -16,14 +16,15 @@ const NotFound = () => {
           as="h2"
           size="2xl"
           bgGradient="linear(to-r, teal.400, teal.600)"
-          backgroundClip="text">
+          backgroundClip="text"
+        >
           404
         </Heading>
         <Text fontSize="18px" mt={3} mb={2}>
-          {t('messages.pageNotFound')}
+          {t("messages.pageNotFound")}
         </Text>
-        <Text color={'gray.500'} mb={6}>
-          {t('messages.pageNotFoundDescription')}
+        <Text color={"gray.500"} mb={6}>
+          {t("messages.pageNotFoundDescription")}
         </Text>
         <Button
           colorScheme="teal"
@@ -32,9 +33,10 @@ const NotFound = () => {
           color="white"
           variant="solid"
           onClick={() => {
-            router.push('/home');
-          }}>
-          {t('buttons.back')} to Home
+            router.push("/home");
+          }}
+        >
+          {t("buttons.back")} to Home
         </Button>
       </Box>
     </Center>
@@ -44,7 +46,7 @@ const NotFound = () => {
 export const getStaticProps: GetStaticProps = async (context) => {
   return {
     props: {
-      ...(await getStaticTranslations(context, ['common'])),
+      ...(await getStaticTranslations(context, ["common"])),
     },
   };
 };
